805f5389a01d08340e8e1850ca65928e1552f600
Shreedhar <shreedharanandji@gmail.com>
2025-03-11 11:25:51 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   server: {
     port: 7195, // Fixed port
     strictPort: true, // Prevents Vite from switching to another port if 7195 is in use
   },
 })
+
